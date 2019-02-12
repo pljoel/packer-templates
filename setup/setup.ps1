86561615bf4e627688875134595b30e3100fdbed
Joel <tijoe9@hotmail.com>
2019-02-11 22:34:28 -0500

    WS2012, WS2019, W10, W7

--- setup/setup.ps1
+++ setup/setup.ps1
@@ -1,9 +1,25 @@
 $ErrorActionPreference = "Stop"
+[double]$osver = [string][environment]::OSVersion.Version.major + '.' + [environment]::OSVersion.Version.minor
 
 # Switch network connection to private mode
 # Required for WinRM firewall rules
-$profile = Get-NetConnectionProfile
-Set-NetConnectionProfile -Name $profile.Name -NetworkCategory Private
+if ($osver -gt 6.1)
+{
+	$profile = Get-NetConnectionProfile
+	Set-NetConnectionProfile -Name $profile.Name -NetworkCategory Private
+}
+elseif ($osver -ge 6)
+{
+	# Start-Sleep 120 # I don't know why but WinRM gets disabled without a sleep
+	$networkListManager = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}"))
+	$connections = $networkListManager.GetNetworkConnections()
+	$connections | % {$_.GetNetwork().SetCategory(1)} #0: Public, 1:Private, 2:Domain
+}
+else
+{
+	echo "Not supported in versions lower than Vista/WS2008"
+	return
+}
 
 # Enable WinRM service
 winrm quickconfig -quiet
--- setup/vmtools.cmd
+++ setup/vmtools.cmd
@@ -1,3 +1,10 @@
-@rem Silent mode, basic UI, no reboot
-e:\setup64 /s /v "/qb REBOOT=R"
-
+@REM https://docs.microsoft.com/en-us/windows/desktop/msi/command-line-options
+@REM https://docs.vmware.com/en/VMware-Tools/10.3.0/com.vmware.vsphere.vmwaretools.doc/GUID-E45C572D-6448-410F-BFA2-F729F2CDA8AC.html
+@REM setup64.exe /S /v "/qn REBOOT=R ADDLOCAL=ALL REMOVE=Hgfs,Sync,Audio,Unity,PerfMon,WYSE,BootCamp,Debug,ThinPrint"
+@REM /l <logfile> - Where to output the logfile
+@REM /S – Silent, non-GUI installation
+@REM /v  – pass parameters directly to MSI
+@REM REBOOT=R – ReallySuppress the server reboot. Note The installer may indicate if a reboot is necessary by exiting with ERROR_SUCCESS_REBOOT_REQUIRED.
+@REM ADDLOCAL=ALL – Install all components locally
+@REM REMOVE=component(s) – Component or components (separated by comma) that will NOT be installed / excluded
+e:\setup64 /s /v "/qn REBOOT=R"
